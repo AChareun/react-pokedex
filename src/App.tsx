c7ba553fef02d12d39872eb5197290c22d12be1b
AChareun <achareun22@gmail.com>
2021-01-27 16:24:40 -0300

    imports on App component and changes to css

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
+
 import styles from './App.module.scss';
+import Header from './component/Header/Header';
+import Footer from './component/Footer/Footer';
+import ItemList from './component/ItemList/ItemList';
 
 function App() {
   return (
-    <div className={styles.App}>
-      <h1>Hello, World!</h1>
+    <div className={styles['App']}>
+      <Header />
+      <ItemList />
+      <Footer />
     </div>
   );
 }
