6f4bc98903ae18e7bc4b956099cbf97677581b4f
AChareun <achareun22@gmail.com>
2021-01-27 16:23:04 -0300

    implement basic item card to display pokemons

---
+++ src/component/Item/Item.tsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from "react";
+
+import styles from "./Item.module.scss";
+
+const Item: React.FC<{ name: string; url: string }> = (props) => {
+	const [pokemon, setPokemon] = useState<{ sprites: {front_default: any} }>({
+		sprites: {front_default: ''},
+	});
+	const { name, url } = props;
+
+	const getPokemonData = async (): Promise<void> => {
+		const pokemonData = await fetch(url).then((r) => r.json());
+
+		setPokemon(pokemonData);
+	};
+
+	useEffect(() => {
+		getPokemonData();
+	}, []);
+
+	const { sprites: {front_default: sprite} } = pokemon;
+
+	return (
+		<article className={styles["Item"]}>
+			<img src={sprite} alt={name} />
+			<div>
+				<h4>{name}</h4>
+			</div>
+		</article>
+	);
+};
+
+export default Item;
