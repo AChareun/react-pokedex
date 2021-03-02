89d91dccdddd96c53638ab4ac7c26b8acb202edc
AChareun <achareun22@gmail.com>
2021-03-02 16:06:22 -0300

    add PokemonType components to Item component

--- src/component/Item/Item.tsx
+++ src/component/Item/Item.tsx
@@ -2,12 +2,11 @@ import React, {useCallback, useEffect, useState} from "react";
 import { Link } from "react-router-dom";
 
 import styles from "./Item.module.scss";
+import {IPokemon} from "../../typings/general";
+import PokemonType from "../PokemonType/PokemonType";
 
 const Item: React.FC<{ name: string; url: string }> = (props) => {
-	const [pokemon, setPokemon] = useState<{ id: any, sprites: {front_default: any} }>({
-		id: '',
-		sprites: {front_default: ''},
-	});
+	const [pokemon, setPokemon] = useState<IPokemon>();
 	const { name, url } = props;
 
 	const getPokemonData = useCallback(async (): Promise<void> => {
@@ -20,20 +19,32 @@ const Item: React.FC<{ name: string; url: string }> = (props) => {
 		getPokemonData().catch(e => console.log(e));
 	}, [url, getPokemonData]);
 
-	const { id, sprites: {front_default: sprite} } = pokemon;
+	if (pokemon) {
+		const { id, sprites, types } = pokemon && pokemon;
 
-	return (
-		<article className={styles["Item"]}>
-			<img src={sprite} alt={name} />
-			<div>
-				<h4>
-					<Link to={{pathname: `/pokemon/${id}`, state: {pokemon}}}>
-						{name}
-					</Link>
-				</h4>
-			</div>
-		</article>
-	);
+		return (
+			<article className={styles["Item"]}>
+				<img src={sprites?.front_default || ''} alt={name} />
+				<div>
+					<h4>
+						<Link to={{pathname: `/pokemon/${id}`, state: {pokemon}}}>
+							{name}
+						</Link>
+					</h4>
+				</div>
+				<div>
+					{types?.map((e, i) => {
+						return (
+							<PokemonType name={e.type.name} key={i}/>
+						)
+					})}
+				</div>
+			</article>
+		);
+	}
+
+	return <React.Fragment></React.Fragment>
+
 };
 
 export default Item;
