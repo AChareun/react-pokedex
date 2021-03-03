47f3a7a0d3ded9b034688d816d36528095f5640b
AChareun <achareun22@gmail.com>
2021-03-03 11:33:26 -0300

    implement Pokemon entity and mapper to abstract from PokeApi

--- src/typings/general.d.ts
+++ src/typings/general.d.ts
@@ -1,18 +1,32 @@
+export interface IApiPokemon {
+    abilities: null | {ability: { name: string }}[];
+    height: number;
+    weight: number;
+    id: number;
+    name: string;
+    sprites: {
+        back_default: string,
+        back_female: string | null,
+        back_shiny: string | null,
+        back_shiny_female: string | null,
+        front_default: string,
+        front_female: string | null,
+        front_shiny: string | null,
+        front_shiny_female: string | null,
+    };
+    types: null | {slot: number, type: {name: string, url: string}}[];
+}
+
 export interface IPokemon {
     abilities: null | {ability: { name: string }}[];
     height: number;
     weight: number;
     id: number;
     name: string;
-    sprites: null | {
-        back_default: string | null;
-        back_female: string | null;
-        back_shiny: string | null;
-        back_shiny_female: string | null;
-        front_default: string | null;
-        front_female: string | null;
-        front_shiny: string | null;
-        front_shiny_female: string | null;
-    };
+    sprites: null | IPokemonSprite;
     types: null | {slot: number, type: {name: string, url: string}}[];
+}
+
+export interface IPokemonSprite {
+    [key: string]: string | null
 }
